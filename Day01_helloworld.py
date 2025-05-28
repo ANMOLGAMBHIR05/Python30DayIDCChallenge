84a72cbb7f676bdc59bf8af468e94a742e3f524a
Anmol Gambhir <anmolgambhir2001@gmail.com>
2025-05-28 23:40:59 +0530

    Day 1 – Hello World program added

---
+++ Day01_helloworld.py
@@ -0,0 +1,6 @@
+#DAY 1 Python IDC CHALLENGE
+
+#Write a program that prints "Hello World !" followed by your name.
+
+print("Hello World ! I am Anmol Gambhir -really excited for this python learning")
+
